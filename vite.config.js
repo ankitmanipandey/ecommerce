3e929d05fa2c58e125bcf1111f23a3e417913f5d
Ankit Mani Pandey <edumusicindia.tech@gmail.com>
2026-07-22 19:07:00 +0530

    made pwa

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,38 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate', // Automatically updates the app when you push new code
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Loomzo India',
+        short_name: 'Loomzo',
+        description: 'Handcrafted authentic sarees and ethnic wear.',
+        theme_color: '#ffffff', // The color of the status bar on mobile
+        background_color: '#ffffff',
+        display: 'standalone', // Makes it look like a native app (hides browser URL bar)
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
